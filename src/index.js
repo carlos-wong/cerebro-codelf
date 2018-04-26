cc33d9b190f182249bf91f8d71c7536def9b8906
carlos <huaixian.huang@gmail.com>
2018-04-26 16:21:27 +0800

    Feat: parse command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Preview = require('./preview');
 import axios from 'axios';
+var lodash = require('lodash');
 
 var instance = axios.create({
     baseURL: 'http://fanyi.youdao.com/openapi.do?callback=?&keyfrom=Codelf&key=2023743559&type=data&doctype=jsonp&version=1.1',
@@ -9,17 +10,24 @@ var instance = axios.create({
 
 export const fn = ({ term, display }) => {
     // Put your plugin code here
-    instance.get('',{params: {q: '你好' }})
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-    display({
-        title: "hicarlos",
-        getPreview: () => <Preview data={{a:1}}  />
+    var split_contents = term.split(" ");
+    if(split_contents[0] == 'codelf'){
+        // console.log('split contents is:', split_contents);
+        var search_contents = lodash.slice(split_contents,1);
+        console.log('search contents:',search_contents.join(" "));
+        display({
+            title: "hicarlos",
+            getPreview: () => <Preview data={{a:1}}  />
         },
-    );
+               );
+    }
+    // instance.get('',{params: {q: '你好' }})
+    //     .then(function (response) {
+    //         console.log(response);
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
+
+    
 };
--- src/preview.js
+++ src/preview.js
@@ -1,7 +1,6 @@
 const React = require('react');
 
 module.exports = (data) => {
-    console.log('dump data is:',data);
     return (
         <div >
          <a>hicarlos</a>
