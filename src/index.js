979cf89b50a4d4daa16857f2bac151b4491be092
carlos <huaixian.huang@gmail.com>
2018-04-26 16:11:47 +0800

    Feat: add axios for youdao

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const Preview = require('./preview');
+import axios from 'axios';
+
+var instance = axios.create({
+    baseURL: 'http://fanyi.youdao.com/openapi.do?callback=?&keyfrom=Codelf&key=2023743559&type=data&doctype=jsonp&version=1.1',
+    timeout: 10000,
+    headers: {'X-Custom-Header': 'foobar'}
+});
 
 export const fn = ({ term, display }) => {
     // Put your plugin code here
+    instance.get('',{params: {q: '你好' }})
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
     display({
         title: "hicarlos",
         getPreview: () => <Preview data={{a:1}}  />
