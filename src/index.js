5a5d63b478da420d5ae82791dae3af34362f756f
carlos <huaixian.huang@gmail.com>
2018-04-29 23:25:54 +0800

    Refactor: clean log

--- src/index.js
+++ src/index.js
@@ -29,11 +29,8 @@ var async_handle_event = async function (search_content,display){
         response = await youdao_axios.get('',{params: {q: search_content }});
     }
     else{
-        console.log('by english');
         response.data = {web:[{value:[search_content]}],translation:[search_content]};
     }
-    // console.log('before get search code response data is:',response.data);
-    // console.log('before get search code response data translation is:',response.data.translation[0]);
     var searchcode = ((response.data && response.data.web && response.data.web[0]) && response.data.web[0].value && response.data.web[0].value[0]) || (response.data.translation && response.data.translation[0] ) || "";
     var tdata = response.data;
     if (tdata.basic && tdata.basic.explains) {
@@ -63,8 +60,6 @@ var async_handle_event = async function (search_content,display){
         key.length && key.length > 1 && els.valRegs.push(getKeyWordReg(key));
     });
     els.valRegs.push(getKeyWordReg(searchcode.replace(/\ /ig, '*').toLowerCase()));
-    console.log('user codelf convert is:',els);
-    console.log('try to search code ret is:',searchcode);
     response = await searchcode_axios.get('/',{
         params:{
             q:searchcode,
@@ -72,7 +67,6 @@ var async_handle_event = async function (search_content,display){
             per_page:90,
         }
     });
-    console.log('response is:',response.data.results);
 
     let data = response.data;
     let lineStr = [];
@@ -106,18 +100,12 @@ var async_handle_event = async function (search_content,display){
             }
         }
     });
-    console.log('found key word is:',found_keyword);
     var keyword_array_map = lodash.map(found_keyword,(value,key)=>{
-        // console.log('dump value is:',value,' key is:',key);
         return {found_word:{count:value,name:key}};
     });
-    // console.log('keyworld array map is:',keyword_array_map);
     var keyword_after_sort = lodash.sortBy(keyword_array_map,[(o)=>{
-        console.log('dump sort o is:',o);
         return o.count;}]);
-    // console.log('found keyword is:',keyword_after_sort);
     lodash.map(keyword_after_sort,(value,key)=>{
-        console.log('value is:',value);
         let value_name = value.found_word.name;
         display({
             title: value_name,
@@ -135,9 +123,7 @@ export const fn = ({ term, display }) => {
     // Put your plugin code here
     var split_contents = term.split(" ");
     if(split_contents[0] == 'codelf'){
-        // console.log('split contents is:', split_contents);
         var search_contents = lodash.slice(split_contents,1);
-        // console.log('search contents:',search_contents.join(" "));
         handle_event(search_contents.join(" "),display);
         // searchcode_axios.get('/?q=array',{
         //     params:{
@@ -147,14 +133,12 @@ export const fn = ({ term, display }) => {
         //     }
         // })
         // .then((response)=>{
-        //     console.log(response);
         //     display({
         //         title: "hicarlos",
         //         getPreview: () => {return response.data;}
         //     });
         // })
         // .catch((response)=>{
-        //     console.log(response);
         // }) ;
     }
     
